3dc58490b352c6c20a121129878757a306957ed9
Eduardo Yoshida Reis <eduardoyreis2006@gmail.com>
2025-03-12 09:41:16 -0300

    Add files via upload

---
+++ Aula_1.py
@@ -0,0 +1,9 @@
+nome = input("digite o nome do funcionario ")
+num1 = int(input("digite o primeiro valor "))
+num2 = float(input("Digite o sgundo valor "))
+resultado = num1 + num2
+
+print(f"O nome é {nome}")
+print(f"O primeiro número foi {num1}")
+print(f"O segundo número foi {num2}")
+print(f"O resultado é {resultado}")
