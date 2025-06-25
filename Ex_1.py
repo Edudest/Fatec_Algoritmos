e378920f6436e602653955f8b480622ca374b3b7
Eduardo Yoshida Reis <eduardoyreis2006@gmail.com>
2025-06-25 09:24:33 -0300

    Delete Ex_1.py

--- Ex_1.py
+++
@@ -1,9 +0,0 @@
-nome = input("digite o nome do funcionario ")
-num1 = int(input("digite o primeiro valor "))
-num2 = float(input("Digite o sgundo valor "))
-resultado = num1 + num2
-
-print(f"O nome é {nome}")
-print(f"O primeiro número foi {num1}")
-print(f"O segundo número foi {num2}")
-print(f"O resultado é {resultado}")
